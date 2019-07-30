02d0c556c016c20567fdab593f085155ffc6a89f
Alejandro Rincon <crincon@itexico.net>
2019-07-30 18:03:25 -0500

    first commit

---
+++ app/src/main/java/edu/training/droidbountyhunterkotlin/ui/main/SectionsPagerAdapter.kt
@@ -0,0 +1,32 @@
+package edu.training.droidbountyhunterkotlin.ui.main
+
+import android.content.Context
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import edu.training.droidbountyhunterkotlin.R
+
+
+private val TAB_TITLES = arrayOf(
+    R.string.tab_text_1,
+    R.string.tab_text_2,
+    R.string.tab_text_3
+)
+
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return PlaceholderFragment.newInstance(position + 1)
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return context.resources.getString(TAB_TITLES[position])
+    }
+
+    override fun getCount(): Int {
+        // Show 3 total pages.
+        return 3
+    }
+}

0bf8aa75277435f93c760ca2c68b2595be08cfd2
Alejandro Rincon <crincon@itexico.net>
2019-08-19 12:10:25 -0500

    Lab 04 terminado

--- app/src/main/java/edu/training/droidbountyhunterkotlin/DetalleActivity.kt
+++ app/src/main/java/edu/training/droidbountyhunterkotlin/DetalleActivity.kt
@@ -1,16 +1,26 @@
 package edu.training.droidbountyhunterkotlin
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE
 import android.provider.Settings
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import edu.training.droidbountyhunterkotlin.data.DatabaseBountyHunter
 import edu.training.droidbountyhunterkotlin.models.Fugitivo
 import edu.training.droidbountyhunterkotlin.network.NetworkServices
 import edu.training.droidbountyhunterkotlin.network.onTaskListener
+import edu.training.droidbountyhunterkotlin.utils.PictureTools
 import kotlinx.android.synthetic.main.activity_detalle.*
 import org.json.JSONObject
 
@@ -19,6 +29,8 @@ class DetalleActivity : AppCompatActivity(){
     private var UDID: String? = ""
     var fugitivo: Fugitivo? = null
     var database: DatabaseBountyHunter? = null
+    private var direccionImagen: Uri? = null
+    private val REQUEST_CODE_PHOTO_IMAGE = 1787
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -36,12 +48,24 @@ class DetalleActivity : AppCompatActivity(){
         }else{
             etiquetaMensaje.text = "Atrapado!!!"
             botonCapturar.visibility = View.GONE
+            if (fugitivo!!.photo.isNotEmpty()){
+                val bitmap = PictureTools.decodeSampledBitmapFromUri(fugitivo!!.photo,
+                    200,200)
+                pictureFugitive.setImageBitmap(bitmap)
+            }
+
         }
     }
 
     fun capturarFugitivoPresionado(view: View){
         database = DatabaseBountyHunter(this)
         fugitivo!!.status = 1
+        if (fugitivo!!.photo.isEmpty()){
+            Toast.makeText(this,
+                "Es necesario tomar la foto antes de capturar al fugitivo",
+                Toast.LENGTH_LONG).show()
+            return
+        }
         database!!.actualizarFugitivo(fugitivo!!)
         val services = NetworkServices(object: onTaskListener {
             override fun tareaCompletada(respuesta: String) {
@@ -79,5 +103,44 @@ class DetalleActivity : AppCompatActivity(){
                 finish()
             }.show()
     }
+
+    fun OnFotoClick(view: View){
+        if (PictureTools.permissionReadMemmory(this)){
+            obtenFotoDeCamara()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == PictureTools.REQUEST_CODE) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                Log.d("RequestPermissions", "Camera - Granted")
+                obtenFotoDeCamara()
+            } else {
+                Log.d("RequestPermissions", "Camera - Not Granted")
+            }
+        }
+    }
+
+    private fun obtenFotoDeCamara() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        direccionImagen = PictureTools.getOutputMediaFileUri(this, MEDIA_TYPE_IMAGE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, direccionImagen)
+        startActivityForResult(intent, REQUEST_CODE_PHOTO_IMAGE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_PHOTO_IMAGE) {
+            if (resultCode == Activity.RESULT_OK) {
+                fugitivo!!.photo = PictureTools.currentPhotoPath
+                val bitmap = PictureTools
+                    .decodeSampledBitmapFromUri(PictureTools.currentPhotoPath, 200, 200)
+                pictureFugitive.setImageBitmap(bitmap)
+            }
+        }
+    }
+
 
 }
--- app/src/main/java/edu/training/droidbountyhunterkotlin/models/Modelos.kt
+++ app/src/main/java/edu/training/droidbountyhunterkotlin/models/Modelos.kt
@@ -3,21 +3,20 @@ package edu.training.droidbountyhunterkotlin.models
 import android.os.Parcel
 import android.os.Parcelable
 
-data class Fugitivo (
-    val id: Int = 0,
-    var name: String = "",
-    var status: Int = 0) : Parcelable {
+data class Fugitivo @JvmOverloads constructor(val id: Int, var name: String, var status: Int, var photo: String = "") : Parcelable {
 
     constructor(parcel: Parcel) : this(
         parcel.readInt(),
         parcel.readString(),
-        parcel.readInt()
+        parcel.readInt(),
+        parcel.readString()
     )
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeInt(id)
         parcel.writeString(name)
         parcel.writeInt(status)
+        parcel.writeString(photo)
     }
 
     override fun describeContents() = 0
